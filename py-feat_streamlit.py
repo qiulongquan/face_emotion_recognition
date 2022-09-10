75fd5c22f6fa4336ca669458b9833071756e2c06
qiulongquan <qiulongquan@qq.com>
2022-09-10 17:25:21 +0900

    fix

--- py-feat_streamlit.py
+++ py-feat_streamlit.py
@@ -18,11 +18,8 @@ HERE = Path(__file__).parent
 detector = Detector(
     face_model="retinaface",
     landmark_model="mobilenet",
-    # landmark_model = "mobilefacenet",
     au_model="svm",
-    emotion_model="fer",
-    # emotion_model="resmasknet",
-    # facepose_model="img2pose",
+    emotion_model="resmasknet"
 )
 MAX_IMAGE_WIDTH=1000
 IMG_BGR_PATH='stream_img_bgr.jpg'
